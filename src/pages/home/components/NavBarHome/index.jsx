5df5011fe086e3edde397d61a76662da72897645
jn <jeniffer.nogueira.ayres@gmail.com>
2022-11-09 18:08:17 -0300

    ..

--- src/pages/home/components/NavBarHome/index.jsx
+++ src/pages/home/components/NavBarHome/index.jsx
@@ -3,59 +3,54 @@ import './styles.css';
 import { Link } from 'react-router-dom';
 function NavBarHome() {
 
-    //var macc = { lat: 42.1382114, lng: -71.5212585 };
-
-    //var map = new google.maps.Map(
-
-        //document.getElementById('map'), { zoom: 15, center: macc });
-
-    //var marker = new google.maps.Marker({ position: macc, map: map });
-
-
+    
     return (
-        <div className='navbar-main-home'>
-            <div>
-                <br /><br />
-                <h1>Cult-vix</h1>{''}
-            </div>
+        <div className="container">
+            <div className='navbar-main-home'>
+                <div>
+                    <br /><br />
+                    <h1>Cult-vix</h1>{''}
+                </div>
 
-            <div>
+                <div>
+                    <br />
+                    <h1><Link to={'/'}>Inicio</Link></h1>{''}
+                    <br />
+                    <a href='/'>   </a>{''}
+                    <br />
+                </div>
+
+                <map name="asdas"></map>
                 <br />
-                <h1><Link to={'/'}>Inicio</Link></h1>{''}
-                <br />
 
-                <a href='/'>   </a>{''}
-                <br />
-            </div>
-            <div id="map">
-
-                
-
-            </div>
-            <br />
-            <div class="card mb-4">
-                <div class="card-header">Descubra</div>
-                <div class="card-body">
-                    <div class="input-group">
-                        <input class="form-control" type="text" placeholder=" ex: Bebs, Centro de vitoria ..." aria-label="ex: Bebs, Centro de vitoria ..." aria-describedby="button-search" />
+                <div class="card mb-4">
+                    <div class="card-header">Descubra</div>
+                    <div class="card-body">
+                        <div class="input-group">
+                        </div>
+                        <input class="form-control" type="text" placeholder=" ex: Centro de vitoria ..." aria-label="ex: Bebs, Centro de vitoria ..." aria-describedby="button-search" />
                         <button class="btn btn-primary" id="button-search" type="button">Go!</button>
                     </div>
                 </div>
+
+                <br />
+                <iframe width="400" height="400" class="mapa" id="maps"
+                    src="https://maps.google.com/maps?q=vitoria&t=&z=13&ie=UTF8&iwloc=&output=embed" 
+                    title=" | asdasd-
+                        Pontos turísticos | Aerial View" frameborder=""
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media;
+                        gyroscope; picture-in-picture" allowfullscreen />
+                <div>
+                    <br />
+                    <iframe width="860" height="400" class="video" aling="left" id='video'
+                        src="https://www.youtube.com/embed/hLrNckEmcAc"
+                        title="CIDADE DE VITÓRIA-ES | Vitória Espírito Santo Brasil -
+                            Pontos turísticos | Aerial View" frameborder=""
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media;
+                            gyroscope; picture-in-picture" allowfullscreen />
+                </div>
             </div>
-
-            <br />
-            <iframe width="860" height="400" class="video" aling="left"
-                src="https://www.youtube.com/embed/hLrNckEmcAc"
-                title="CIDADE DE VITÓRIA-ES | Vitória Espírito Santo Brasil -
-                      Pontos turísticos | Aerial View" frameborder=""
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media;
-                      gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-
-
         </div>
-
-
     );
 }
 
--- src/pages/home/components/NavBarHome/maps.js
+++
@@ -1,5 +0,0 @@
-
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=8HgtlqKauiuytV6UvwOX1SePyUE=&callback=initMap"></script>
-
-
-//<script async defer src="https://maps.googleapis.com/maps/api/js?key=8HgtlqKauiuytV6UvwOX1SePyUE=&callback=initMap"></script>
